274a2da0f2f96c79fdeea3f78162a9f5e16ca022
Scraps-ftc-25110 <156611960+Scraps-ftc-25110@users.noreply.github.com>
2024-02-04 19:46:25 +0200

    documented PropReco.java

--- current/visualDetection/processor/PropReco.java
+++ current/visualDetection/processor/PropReco.java
@@ -13,18 +13,23 @@ import org.opencv.core.Scalar;
 
 public class PropReco implements VisionProcessor {
 
-
+    
     public static PropReco.AllianceColor alliance = AllianceColor.NONE;
     PropReco.Selected selection = PropReco.Selected.NONE;
 
+    // these 2 line are for the processing of the frame 
+    
     Mat frame = new Mat();
     Mat submat = new Mat();
 
+    // these 3 lines are for the division webcam(video feed) input in 3 seperate parts
+    
     int width = frame.width();
     int height = frame.height();
     int sectionWidth = width / 3;
 
-
+    // the following lines are for the inistialisation of the rectangles
+    
     public Rect rectLeft = new Rect(0, 0, sectionWidth, height);
     public Rect rectMiddle = new Rect(sectionWidth, 0, sectionWidth, height);
     public Rect rectRight = new Rect(sectionWidth * 2, 0, sectionWidth, height);
@@ -33,8 +38,13 @@ public class PropReco implements VisionProcessor {
 
     }
 
+    
+    
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
+
+        // the following lines are for geting the averge RGB values of the RED and BLUE value value contained in the squeres
+        
         double averageRedLeft = getAvgRed(frame, rectLeft);
         double averageBlueLeft = getAvgBlue(frame, rectLeft);
 
@@ -47,12 +57,16 @@ public class PropReco implements VisionProcessor {
         double averageRed = (averageRedLeft + averageRedRight + averageRedMiddle) / 3;
         double averageBlue = (averageBlueLeft + averageBlueRight + averageBlueMiddle) / 3;
 
+        // this statement determens what alliance is the robot placed in
+        
         if(averageRed > averageBlue){
             alliance = AllianceColor.RED;
         }else {
             alliance = AllianceColor.BLUE;
         }
 
+        // these statements are for determening the postition of the prop 
+        
         if (AllianceColor.RED == alliance){
             if((averageRedLeft > averageRedMiddle) && (averageRedLeft > averageRedRight)){
                 return Selected.LEFT;
@@ -70,19 +84,24 @@ public class PropReco implements VisionProcessor {
         }
     }
 
+    // this fanction gets the average RED value of the subsection passed in
+    
     protected double getAvgRed(Mat input, Rect rect) {
         submat = input.submat(rect);
         Scalar color = Core.mean(submat);
         return color.val[0];
     }
 
-
+    // this fanction gets the average BLUE value of the subsection passed in
+    
     protected double getAvgBlue(Mat input, Rect rect) {
         submat = input.submat(rect);
         Scalar color = Core.mean(submat);
         return color.val[2];
     }
 
+    // this function is responsible for drawing the rectangles
+    
     private android.graphics.Rect makeGraphicsRect(Rect rect, float scaleBmpPxToCanvasPx) {
         int left = Math.round(rect.x * scaleBmpPxToCanvasPx);
         int top = Math.round(rect.y * scaleBmpPxToCanvasPx);
@@ -93,20 +112,30 @@ public class PropReco implements VisionProcessor {
     }
 
     @Override
+
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        
+        // these lines handle the colour of the rectangles that the prop is in
+        
         Paint selectedPaint = new Paint();
         selectedPaint.setColor(Color.RED);
         selectedPaint.setStyle(Paint.Style.STROKE);
         selectedPaint.setStrokeWidth(scaleCanvasDensity * 4);
 
+        // these lines handle the colour of the rectangle that the prop is NOT in
+        
         Paint nonSelectedPaint = new Paint(selectedPaint);
         nonSelectedPaint.setColor(Color.GREEN);
 
+        // these lines set the spaces for the rectangles
+        
         android.graphics.Rect drawRectangleLeft = makeGraphicsRect(rectLeft,
                 scaleBmpPxToCanvasPx);
         android.graphics.Rect drawRectangleMiddle = makeGraphicsRect(rectMiddle,
                 scaleBmpPxToCanvasPx);
         android.graphics.Rect drawRectangleRight = makeGraphicsRect(rectRight, scaleBmpPxToCanvasPx);
+
+        // these lines draw the rectangles
 
         selection = (PropReco.Selected) userContext;
         switch (selection) {
